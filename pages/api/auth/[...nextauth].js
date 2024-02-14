31fecb375c7d7bd1626cb1157c4dd4bff1b7df0e
lubuofficial <lilly9101@gmail.com>
2024-02-14 23:19:49 +0700

    deployment

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,6 +13,7 @@ async function isAdminEmail(email) {
 }
 
 export const authOptions = {
+  secret: process.env.SECRET,
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
